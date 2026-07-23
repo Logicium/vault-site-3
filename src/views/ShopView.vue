6b67043a374d9cb7a6a88e045b0d6206a6590b30
Kisora Thomas <kisora.thomas@gmail.com>
2026-07-22 21:07:04 -0600

    chore: sync from template 42c4473

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import { siteConfig } from '../config/site.config'
-import HeroSection from '../components/sections/HeroSection.vue'
+import HeroSection from '@apotome/archetype-shared/components/sections/HeroSection.vue'
+import ShopSection from '@apotome/archetype-shared/components/sections/ShopSection.vue'
 import CategoriesSection from '../components/sections/CategoriesSection.vue'
 import ProductsSection from '../components/sections/ProductsSection.vue'
+import { useSiteContentStore } from '@apotome/archetype-shared/platform/siteContentStore'
+
+const store = useSiteContentStore()
+const showShop = computed(() => store.hasAddOn('eshop'))
 </script>
 
 <template>
@@ -14,6 +20,16 @@ import ProductsSection from '../components/sections/ProductsSection.vue'
     :image="siteConfig.photos.hero.src"
     :image-alt="siteConfig.photos.hero.alt"
   />
-  <CategoriesSection :categories="siteConfig.categories" />
-  <ProductsSection title="Featured" :products="siteConfig.featured" :shop-all-url="siteConfig.shopUrl" />
+
+  <ShopSection
+    v-if="showShop"
+    eyebrow="Shop"
+    title="Available now"
+    intro="Add to cart, then check out for pickup or shipping."
+  />
+
+  <template v-else>
+    <CategoriesSection :categories="siteConfig.categories" />
+    <ProductsSection title="Featured" :products="siteConfig.featured" :shop-all-url="siteConfig.shopUrl" />
+  </template>
 </template>
